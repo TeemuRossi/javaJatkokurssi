1faaf690af73044d3aa6e4b0560e757cafaa1e48
TeemuRossi <33146833+TeemuRossi@users.noreply.github.com>
2018-10-07 20:42:26 +0300

    kommentteja
    vähän kommentteja lisää koodin sekaan

--- JuomaAutomaatti.java
+++ JuomaAutomaatti.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -20,44 +19,54 @@ public class JuomaAutomaatti {
 		return "|Kahvia jäljellä: " + this.kahvia + " | Teetä jäljellä: " + this.teetä + " | Kaakaota jäljellä: "
 				+ this.kaakaota + "|\n";
 	}
+	
+	
+	// luodaan getterit ja setterit
+	
 
+	// jokaisessa alla olevassa methodissa tarkistetaan juoman määrä ja että
+	// voidaanko tätä valmistaa, if-lauseen avulla
+	
 	// valmistetaan vähän kahvia
 	public void valmistaKahvi() {
-		
+
 		if (this.kahvia >= 10) {
 			System.out.println("Odota hetki, Kahvia tulossa.");
-		this.kahvia -= 10;
+			this.kahvia -= 10;
 		} else {
-			
+
 			System.out.println("Kahvia ei ole enää jäljellä! Täytä säiliö");
 		}
 	}
 
 	// valmistetaan vähän teetä
 	public void valmistaTee() {
-		
+
 		if (this.teetä >= 10) {
 			System.out.println("Odota hetki, Teetä tulossa. ");
-		this.teetä -= 10;
+			this.teetä -= 10;
 		} else {
-			
+
 			System.out.println("Teetä ei ole enää jäljellä! Täytä säiliö");
 		}
 	}
 
 	// making the kaakaos
 	public void valmistaKaakao() {
-		
+
 		if (this.kaakaota >= 10) {
 			System.out.println("Odota hetki, Kaakaota tulossa.");
-		this.kaakaota -= 10;
+			this.kaakaota -= 10;
 		} else {
-			
+
 			System.out.println("Kaakaota ei ole enää jäljellä! Täytä säiliö");
 		}
 	}
 
 	// täytyyhän koneesta löytyä mahdollisuus rahan katoamiselle :)
+	// eli luotu math.rand ja katsotaan että onko saatu luku pienempi kuin 25 vai
+	// suurempi ja palautetaan joko true tai false. Sitten tästä riippuen juoma joko
+	// valmistuu tai kone syö käyttäjän rahat
 	public boolean onnistuuko() {
 		int luku = (int) (Math.random() * 100 + 1);
 		if (luku < 25) {
--- JuomaAutomatisoidaan.java
+++ JuomaAutomatisoidaan.java
@@ -9,14 +9,21 @@ public class JuomaAutomatisoidaan {
 		Scanner lukija = new Scanner(System.in);
 		// luodaan juomis
 		JuomaAutomaatti juomaKone = new JuomaAutomaatti();
-		// looppia
+		// looppia jossa ohjelma toimii, kunnes poistutaan (joko kone syö rahat tai
+		// käyttäjä valitsee poistumisen)
 		while (true) {
 			// *-merkillä UI
 			System.out.println("*******Juoma-Automaatti*******\n");
 			System.out.println("1. Kahvi\n2. Tee\n3. Kaakao\n4. Lopeta tilaaminen\n\n******************************");
 
 			int valinta = lukija.nextInt();
-			if (juomaKone.onnistuuko() == true && valinta!=4) {
+			// if lauseella katsotaan "onnistuuko" juoman tekeminen ja otetaan huomioon valinta 4, jolloin
+			// käyttäjä haluaa itse poistua, ettei tähän tule ilmoitusta syödystä rahasta
+			
+			// normaalisti tämän jälkeen valinnasta riippuen annetaan joko tee/kahvi/kaakao
+			// kyseistä metodia kutsuen, lopuksi kutsutaan juomakone toStringiä, jolla
+			// saadaan jäljellä olevat juomamäärät näkyviin
+			if (juomaKone.onnistuuko() == true && valinta != 4) {
 				if (valinta == 1) {
 					juomaKone.valmistaKahvi();
 					System.out.println(juomaKone);
